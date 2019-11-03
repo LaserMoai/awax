f67d1725f6eaeb8fbafd916f13202329dbb686a8
LaserMoai <just4steam778@gmail.com>
2019-11-03 20:15:49 +0300

    Fix markup

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,5 @@
 function handleFirstTab(e) {
 	if (e.keyCode === 9) {
-		// the "I am a keyboard user" key
 		document.body.classList.add("user-is-tabbing");
 		window.removeEventListener("keydown", handleFirstTab);
 	}
